d4578425d918e8439c5bd803dc0f7f42fe13fc87
amatnrr <147020387+amatnrr@users.noreply.github.com>
2023-12-11 05:52:31 +0700

    Add files via upload

---
+++ pekan 11/aritmatika.py
@@ -0,0 +1,45 @@
+def penjumlahan(angka1,angka2):
+    hasil = angka1+angka2
+    print ("Hasil penjumlahan: ",hasil)
+    
+def pengurangaran(angka1,angka2):
+    hasil = angka1-angka2
+    print ("Hasil pengurangan: ",hasil)
+    
+def perkalian(angka1,angka2):
+    hasil = angka1*angka2
+    print ("Hasil perkalian: ",hasil)
+    
+def pembagian(angka1,angka2):
+    hasil = angka1/angka2
+    print ("Hasil pembagian: ", hasil)
+    
+def pangkat(angka1,angka2):
+    hasil = angka1**angka2
+    print ("Hasil pangkat: ", hasil)
+    
+def kuadrat(angka1):
+    kuadrat = angka1**2
+    print ("Hasil kuadrat: ", kuadrat)
+    
+def mod(angka1,angka2):
+    hasil = angka1%angka2
+    print ("Hasil mod: ", hasil)
+    
+def log (angka1,angka2):
+    hasil = angka1**angka2
+    print ("Hasil log: ", hasil)
+    
+def faktorial(angka1):
+    faktorial = 1
+    for i in range(1,angka1+1):
+        faktorial = faktorial*i
+    print ("Hasil faktorial: ", faktorial)
+    
+def trigonometric(angka1):
+    sinus = angka1
+    cosinus = angka1
+    tangen = angka1
+    print ("Hasil sinus: ", sinus)
+    print ("Hasil cosinus: ", cosinus)
+    print ("Hasil tangen: ", tangen)
---
+++ pekan 11/bangundatar.py
@@ -0,0 +1,49 @@
+def persegi (sisi):
+    luas = sisi * sisi
+    keliling = 4 * sisi
+    print("luas persegi: ",luas)
+    print("keliling persegi:",keliling)
+    
+def persegi_panjang(panjang, lebar):
+        luas = panjang * lebar
+        keliling = 2 * (panjang + lebar) 
+        print("luas persegi panjang: ", luas)
+        print("keliling persegi panjang: ", keliling)
+         
+def segitiga_sama_sisi(alas, tinggi):
+        luas = 0.5 * alas * tinggi
+        keliling = 3 * alas
+        print("luas segitiga sama sisi: ", luas)
+        print("keliling segitiga sama sisi: ", keliling)
+        
+def jajar_genjang(alas, tinggi, sisi_miring):
+        luas = alas * tinggi
+        keliling = (2 * alas) + (2 * sisi miring)
+        print("luas jajar genjang: ", luas)
+        print("keliling jajar genjang: ", keliling)
+        
+def lingkaran(jari2):
+        phi=3.14
+        luas = phi * jari2 * jari2
+        luas = phi * jari2 ** 2
+        keliling = 2 * jari2 * phi
+        print("lingkaran: ", luas)
+        print("lingkaran: ", keliling)
+        
+def belah_ketupat(diagonal_1, diagonal_2, sisi):
+        luas = 0.5 * diagonal_1 * diagonal_2
+        keliling = 4 * sisi
+        print("luas belah ketupat: ", luas)
+        print("keliling belah ketupat: ", keliling)
+        
+def layang_layang(diagonal_1, diagonal_2, sisi_atas, sisi_bawah):
+        luas = 0.5 * diagonal_1 * diagonal_2
+        keliling = (2 * sisi_atas) + (2 * sisi_bawah )
+        print("luas layang-layang: ", luas)
+        print("keliling layang-layang: ", keliling)
+
+        
+
+        
+        
+         
